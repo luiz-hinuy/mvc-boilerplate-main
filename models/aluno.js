f3123a5f8c290bb09d97ba2c49449a1a123711d9
luiz-hinuy <luiz.hinuy@sou.inteli.edu.br>
2025-05-22 11:04:28 -0300

    feat Adiciona partes 1 e 2 da aula da semana 5

--- models/aluno.js
+++ models/aluno.js
@@ -2,28 +2,6 @@ const db = require('../config/db');
 
 module.exports = {
   async create(data) {
-    const query = 'INSERT INTO aluno (nome, email) VALUES ($1, $2)';
-    const values = [data.nome, data.email];
-    return db.query(query, values);
-  },
-
-  async findAll() {
-    const result = await db.query('SELECT * FROM aluno ORDER BY id ASC');
-    return result.rows;
-  },
-
-  async update(id, data) {
-    const query = 'UPDATE aluno SET nome = $1, email = $2 WHERE id = $3';
-    const values = [data.nome, data.email, id];
-    return db.query(query, values);
-  },
-
-  async delete(id) {
-    const query = 'DELETE FROM aluno WHERE id = $1';
-    return db.query(query, [id]);
-  },
-
-  async create(data) {
     const query = 'INSERT INTO aluno (nome, email, curso_id) VALUES ($1, $2, $3)';
     const values = [data.nome, data.email, data.curso_id || null];
     return db.query(query, values);
@@ -41,13 +19,19 @@ module.exports = {
   },
 
   async findByCurso(curso_id) {
-    const query = `
-      SELECT aluno.id, aluno.nome, aluno.email
-      FROM aluno
-      WHERE curso_id = $1
-      ORDER BY nome ASC
-    `;
+    const query = 'SELECT aluno.id, aluno.nome, aluno.email FROM aluno WHERE curso_id = $1 ORDER BY nome ASC';
     const result = await db.query(query, [curso_id]);
     return result.rows;
+  },
+
+  async update(id, data) {
+    const query = 'UPDATE aluno SET nome = $1, email = $2 WHERE id = $3';
+    const values = [data.nome, data.email, id];
+    return db.query(query, values);
+  },
+
+  async delete(id) {
+    const query = 'DELETE FROM aluno WHERE id = $1';
+    return db.query(query, [id]);
   }
 };
